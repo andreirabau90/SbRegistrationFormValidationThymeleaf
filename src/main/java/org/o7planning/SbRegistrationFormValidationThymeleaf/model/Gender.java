30ac4f4ca953c7128f63f92c7c994ff81cb8a663
Andrei Rabau <andreyrabov90@gmail.com>
2019-12-05 11:11:10 +0300

    Initial commit

---
+++ src/main/java/org/o7planning/SbRegistrationFormValidationThymeleaf/model/Gender.java
@@ -0,0 +1,6 @@
+package org.o7planning.SbRegistrationFormValidationThymeleaf.model;
+
+public class Gender {
+    public static final String MALE = "M";
+    public static final String FEMALE = "F";
+}
